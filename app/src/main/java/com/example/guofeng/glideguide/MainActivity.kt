8428fa1a9b3380fd432c274fc9fa132f51a1862f
guofeng <imeepwni@outlook.com>
2017-10-11 08:20:44 +0800

    1）移除无用引用。

--- app/src/main/java/com/example/guofeng/glideguide/MainActivity.kt
+++ app/src/main/java/com/example/guofeng/glideguide/MainActivity.kt
@@ -6,9 +6,12 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
@@ -73,7 +76,23 @@ class MainActivity : AppCompatActivity() {
                     .miniThumb()
                     .into(imageView)
         }
-
+        request_load.setOnClickListener {
+            Glide.with(this)
+                    .load(url)
+                    .apply(RequestOptions.centerCropTransform())
+                    // apply() 方法可以被调用多次，因此 RequestOption 可以被组合使用。
+                    // 如果 RequestOptions 对象之间存在相互冲突的设置，那么只有最后一个被应用的 RequestOptions 会生效。
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(imageView)
+        }
+        transform_load.setOnClickListener {
+            GlideApp.with(this)
+                    .load(url)
+                    // 不同于RequestOptions，TransitionOptions是特定资源类型独有的，
+                    // 你能使用的变换取决于你让Glide加载哪种类型的资源。
+                    .transition(DrawableTransitionOptions.withCrossFade(1000))
+                    .into(imageView)
+        }
     }
 
 }
