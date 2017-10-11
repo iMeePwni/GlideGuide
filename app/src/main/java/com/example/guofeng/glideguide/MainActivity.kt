e7913f89148676ed3f830d9058c97101aa5a4452
guofeng <imeepwni@outlook.com>
2017-10-11 21:59:44 +0800

    添加变换Demo。

--- app/src/main/java/com/example/guofeng/glideguide/MainActivity.kt
+++ app/src/main/java/com/example/guofeng/glideguide/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
@@ -34,9 +35,11 @@ class MainActivity : AppCompatActivity() {
                     .error(R.mipmap.ic_launcher_foreground)
                     // 后备回调符。在请求的url/model为null时显示。null也可能表示这个原数据根本就是不合法的，或者取不到的。
                     .fallback(R.mipmap.ic_launcher_round)
+                    // 注意多重变换 每个transform()调用，或任何特定转换方法fitCenter()、centerCrop()、bitmapTransform()
+                    // 等等的调用会替换之前的变换。
+                    // 如果你想在单次加载中应用多个变换，请使用MultiTransformation类。
+                    .transform(FitCenter())
                     .into(imageView)
-
-
         }
         custom_load.setOnClickListener {
             GlideApp.with(this)
